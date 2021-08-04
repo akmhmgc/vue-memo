5f04d552c443308512331fb8d453d8a5065d5573
akmhmgc <akimuhamaguchi@icloud.com>
2021-08-04 22:08:25 +0900

    Add メモの投稿API追加

--- app/controllers/api/memos_controller.rb
+++ app/controllers/api/memos_controller.rb
@@ -4,17 +4,17 @@ class Api::MemosController < ApplicationController
   end
 
   def create
-    # @memo = Memo.new(memo_params)
-    # if @memo.save
-    #     render: :show, status: :created
-    # else
-    #     render json: @memo.errors, status: :unprocessable_entity
-    # end
+    @memo = Memo.new(memo_params)
+    if @memo.save
+        render :show, status: :created
+    else
+        render json: @memo.errors, status: :unprocessable_entity
+    end
   end
 
   private
 
-  def memo_parms
+  def memo_params
     params.permit(:title, :description)
   end
 end
