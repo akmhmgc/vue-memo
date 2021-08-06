a907f6c48fedd409eb7a3024166298d3ea7364c3
Akimu Hamaguchi <akimuhamaguchi@icloud.com>
2021-08-06 09:55:27 +0900

    windowsに対応

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,11 @@
 FROM ruby:2.7
 
-ENV RAILS_ENV=production
-
 RUN curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - \
   && echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list \
   && curl -sL https://deb.nodesource.com/setup_lts.x | bash - \
   && apt-get update -qq \
   && apt-get install -y nodejs yarn \
-  && yarn add axios
+  && yarn install
 WORKDIR /app
 COPY . /app
 RUN bundle config --local set path 'vendor/bundle' \
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_804_060_934) do
-  create_table 'memos', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci',
-                        force: :cascade do |t|
-    t.string 'title'
-    t.text 'description'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+ActiveRecord::Schema.define(version: 2021_08_04_060934) do
+
+  create_table "memos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
+
 end
