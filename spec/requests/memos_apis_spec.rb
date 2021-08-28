f6ab5165d7d5652782b44bf3b5dd2665bbf1189b
akmhmgc <akimuhamaguchi@icloud.com>
2021-08-28 16:22:30 +0900

    Api/MemosController#indexのテスト追加

--- spec/requests/memos_apis_spec.rb
+++ spec/requests/memos_apis_spec.rb
@@ -2,9 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "MemosApis", type: :request do
   describe "GET /memos_apis" do
+    before do 
+      FactoryBot.create_list(:memo, 5)
+    end
     it "メモを読み出すこと" do
-      get home_path
+      get api_memos_path
       expect(response).to have_http_status(200)
+
+      json = JSON.parse(response.body)
+      expect(json.length).to eq 5
     end
   end
 end
